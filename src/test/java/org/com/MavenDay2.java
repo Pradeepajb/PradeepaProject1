377ffd2a72704435f6ba3acc2bef089cead1cdde
Pradeepajb <pradeepapradeepa358@gmail.com>
2022-08-18 21:11:34 +0530

    new code added by PJ1#2

--- src/test/java/org/com/MavenDay2.java
+++ src/test/java/org/com/MavenDay2.java
@@ -4,12 +4,18 @@ package org.com;
 
 public class MavenDay2 {
 	public static void main(String[] args)  {
-	
-		
-		System.out.println("Test Line 1");
+	System.out.println("Test Line 1");
 		System.out.println("Test Line 2");
 		System.out.println("Test Line 3");
 		System.out.println("Test Line 4");
+		System.out.println("Test Line 4");
+		System.out.println("Test Line 4");
+		System.out.println("Test Line 4");
+		System.out.println("Test Line 4");
+		System.out.println("Test Line 4");
+		
+		
+		
 
 }
 
