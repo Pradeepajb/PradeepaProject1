b99ee367b603c1a3beca45e208ec40b821b6d7db
Pradeepajb <pradeepapradeepa358@gmail.com>
2022-08-18 21:03:26 +0530

    New code Added by PJ2

--- src/test/java/org/com/MavenDay2.java
+++ src/test/java/org/com/MavenDay2.java
@@ -1,39 +1,14 @@
 package org.com;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MavenDay2 {
-	public static void main(String[] args) throws IOException {
-		
-		
-File f=new File("C:\\Users\\Pradeepa\\eclipse-workspace\\Pradeepa\\Excel\\MavenSample1.xlsx");
+	public static void main(String[] args)  {
+	System.out.println("Line updated by PJ2");
+	System.out.println("Line updated by PJ2");	
+	System.out.println("Line updated by PJ2");	
+	System.out.println("Line updated by PJ2");	
+	System.out.println("Line updated by PJ2");	
 
-FileInputStream fin = new FileInputStream(f);
-
-Workbook w = new XSSFWorkbook(fin);
-
-Sheet s = w.getSheet("Sheet1");
-
-Row r = s.getRow(0);
-
-Cell c = r.getCell(1);
-
-System.out.println(c);
- for (Cell cell : r) {
-	 System.out.println(cell);
-	 
-	 System.out.println("Updated by PJ2");
 
 }
 
@@ -42,4 +17,4 @@ System.out.println(c);
 	
 	
 
-}
+
